5e7eeac25f4eaf7f252f21c77036afd4addf1305
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-02-27 21:14:08 +0530

    adding account repository

---
+++ src/main/java/com/springframework/banking_app/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.springframework.banking_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springframework.banking_app.entity.Account;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,Long>{
+    
+}
