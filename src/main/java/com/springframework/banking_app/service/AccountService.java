71634104e28977f4a29fd5c24324d7f55e0c579d
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-02-28 14:01:58 +0530

    adding getAccountById method & it's impl

--- src/main/java/com/springframework/banking_app/service/AccountService.java
+++ src/main/java/com/springframework/banking_app/service/AccountService.java
@@ -5,4 +5,6 @@ import com.springframework.dto.AccountDto;
 public interface AccountService {
     
     AccountDto createAccount(AccountDto account);
+
+    AccountDto getAccountById(Long id);
 }
--- src/main/java/com/springframework/banking_app/service/AccountServiceImpl.java
+++ src/main/java/com/springframework/banking_app/service/AccountServiceImpl.java
@@ -1,10 +1,15 @@
 package com.springframework.banking_app.service;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
 import com.springframework.banking_app.entity.Account;
 import com.springframework.banking_app.repository.AccountRepository;
 import com.springframework.dto.AccountDto;
 import com.springframework.mapper.AccountMapper;
 
+@Service
 public class AccountServiceImpl implements AccountService{
 
     private AccountRepository accountRepository;
@@ -18,6 +23,13 @@ public class AccountServiceImpl implements AccountService{
         
         Account savedAccount = accountRepository.save(account);
         return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    public AccountDto getAccountById(Long id) {
+        Account account = accountRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Account does not exists"));
+
+        return AccountMapper.mapToAccountDto(account);
     }
     
 }
