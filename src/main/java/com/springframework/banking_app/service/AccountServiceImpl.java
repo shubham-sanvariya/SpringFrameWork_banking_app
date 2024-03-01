e2203ffac01de4cc77094833eca7738ee4cd2ef2
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-03-01 15:27:51 +0530

    adding deposit method & it's impl

--- src/main/java/com/springframework/banking_app/service/AccountServiceImpl.java
+++ src/main/java/com/springframework/banking_app/service/AccountServiceImpl.java
@@ -27,9 +27,18 @@ public class AccountServiceImpl implements AccountService{
 
     public AccountDto getAccountById(Long id) {
         Account account = accountRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Account does not exists"));
+                .orElseThrow(() -> new RuntimeException("Account not found"));
 
         return AccountMapper.mapToAccountDto(account);
+    }
+
+    public AccountDto deposit(Long id, double amount) {
+        Account account =  accountRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Account not found"));
+        double updatedAmount = account.getBalance() + amount;
+        account.setBalance(updatedAmount);
+        Account updatedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(updatedAccount);
     }
     
 }
