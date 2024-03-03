2c6141a10ca493dc82b3e95e0058650967c5feea
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-03-03 12:37:47 +0530

    adding put mapping for withdrawal

--- src/main/java/com/springframework/banking_app/controller/AccountController.java
+++ src/main/java/com/springframework/banking_app/controller/AccountController.java
@@ -46,4 +46,13 @@ public class AccountController {
         AccountDto accountDto = accountService.deposit(id,amount);
         return ResponseEntity.ok(accountDto);
     }
+
+    @PutMapping("/{id}/withdraw")
+    public ResponseEntity<AccountDto> withdraw(@PathVariable Long id,
+        @RequestBody Map<String,Double> map){
+        
+        double amount = map.get("amount");
+        AccountDto accountDto = accountService.withdraw(id,amount);
+        return ResponseEntity.ok(accountDto);
+    }
 }
